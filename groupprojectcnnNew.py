e726127672faa9a7601c8c0790f78ceddbc04052
AyaBassel3 <87432856+AyaBassel3@users.noreply.github.com>
2023-07-12 18:47:18 +0200

    Update groupprojectcnnNew.py

--- groupprojectcnnNew.py
+++ groupprojectcnnNew.py
@@ -114,7 +114,7 @@ output = tf.keras.layers.Dense(15, activation='softmax')(x)
 model = Model(inputs=pretrained_model.input, outputs=output)
 #model.summary()
 
-model.compile(optimizer='adam', loss=BiTemperedLogisticLoss(t1=1, t2=1), metrics=['accuracy'])
+model.compile(optimizer='adam', loss=tf.losses.SigmoidFocalCrossEntropy(), metrics=['accuracy'])
 
 
 
