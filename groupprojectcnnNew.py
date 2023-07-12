1d3c7194e54b207e98098bab642bd9b200308621
AyaBassel3 <87432856+AyaBassel3@users.noreply.github.com>
2023-07-12 09:08:52 +0200

    Update groupprojectcnnNew.py

--- groupprojectcnnNew.py
+++ groupprojectcnnNew.py
@@ -125,7 +125,7 @@ output = tf.keras.layers.Dense(15, activation='softmax')(x)
 model = Model(inputs=pretrained_model.input, outputs=output)
 #model.summary()
 
-model.compile(optimizer=tf.optimizers.Adam(learning_rate=0.01, loss=BiTemperedLogisticLoss(t1=0.9, t2=1.5), metrics=['accuracy'])
+model.compile(optimizer=tf.optimizers.Adam(learning_rate=0.01), loss=BiTemperedLogisticLoss(t1=0.9, t2=1.5), metrics=['accuracy'])
 
 
 
