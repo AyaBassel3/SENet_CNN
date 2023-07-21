00ab82501ddfc0c70462e4c8b314deb35b352cdb
AyaBassel3 <87432856+AyaBassel3@users.noreply.github.com>
2023-07-21 10:52:50 +0200

    Update groupprojectcnnNew.py

--- groupprojectcnnNew.py
+++ groupprojectcnnNew.py
@@ -132,7 +132,7 @@ train_datagen = ImageDataGenerator(
 def squeeze_excite_block2D(filters, input):
     se = tf.keras.layers.GlobalAveragePooling2D()(input)
     se = tf.keras.layers.Reshape((1, filters))(se)
-    se = tf.keras.layers.Dense(filters // 32, activation='relu')(se)
+    se = tf.keras.layers.Dense(filters * 4, activation='relu')(se)
     se = tf.keras.layers.Dense(filters, activation='sigmoid')(se)
     se = tf.keras.layers.multiply([input, se])
     return se
@@ -170,13 +170,13 @@ x = Conv2D(filters, 3, activation='relu', padding='same')(x)
 x = Conv2D(filters, 3, activation='relu', padding='same')(x)
 x = Dropout(0.8)(x)
 x = BatchNormalization()(x)
-x = squeeze_excite_block2D(filters*4, x)
+x = squeeze_excite_block2D(filters, x)
 x = Dropout(0.5)(x)
 #x = tf.keras.layers.concatenate([tf.keras.layers.GlobalMaxPooling2D()(x),
 #                                tf.keras.layers.GlobalAveragePooling2D()(x)])
 x = tf.keras.layers.Flatten()(x)
 #x = tf.keras.layers.Dense(1024, activation='relu')(x)
-x = tf.keras.layers.Dense(512, activation='relu')(x)
+x = tf.keras.layers.Dense(128, activation='relu')(x)
 output = tf.keras.layers.Dense(15, activation='softmax')(x)
 
 # Create the new model
