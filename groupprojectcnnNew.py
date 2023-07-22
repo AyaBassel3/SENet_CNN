7de7bb5ed48edffc1156cb67d3d84ace660390df
AyaBassel3 <87432856+AyaBassel3@users.noreply.github.com>
2023-07-22 21:51:52 +0200

    Update groupprojectcnnNew.py

--- groupprojectcnnNew.py
+++ groupprojectcnnNew.py
@@ -98,14 +98,14 @@ model.compile(optimizer=SGD(momentum=0.9), loss=BiTemperedWrapper(t1=0.97,t2=1.0
 
 
 
-model.fit(
-    train_generator,
-    steps_per_epoch=len(train_generator),
-    epochs=100,
-    validation_data=test_generator,
-    validation_steps=len(test_generator),
-    callbacks=[checkpoint]
-)
+#model.fit(
+#    train_generator,
+#    steps_per_epoch=len(train_generator),
+#    epochs=100,
+#    validation_data=test_generator,
+#    validation_steps=len(test_generator),
+#    callbacks=[checkpoint]
+#)
 
 
 model.load_weights('best_DenseNet_model64')
@@ -184,6 +184,7 @@ x = tf.keras.layers.Flatten()(x)
 #x = tf.keras.layers.Dense(1024, activation='relu')(x)
 x = tf.keras.layers.Dense(512, activation='relu')(x)
 x = Dropout(0.5)(x)
+x = tf.keras.layers.Dense(128, activation='relu')(x)
 output = tf.keras.layers.Dense(15, activation='softmax')(x)
 
 # Create the new model
